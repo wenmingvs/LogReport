79b4d0bf0c8cc646c2d054bada9aab0214cd3197
littlecurl <424171723@qq.com>
2020-02-16 17:15:46 +0800

    迁移到AndroidX，升级gradle

--- app/src/main/java/com/wenming/crashcachedemo/MainActivity.java
+++ app/src/main/java/com/wenming/crashcachedemo/MainActivity.java
@@ -1,9 +1,10 @@
 package com.wenming.crashcachedemo;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.wenming.library.LogReport;
 import com.wenming.library.save.imp.LogWriter;
